f9d52569187c460480ff71431cc077764dc17524
Alcapone-Fx <navasfx19@gmail.com>
2019-03-24 22:59:59 -0600

    Creacion de activity para el ingreso de un nuevo paciente, se utilizan los colores que tendra la app
    Signed-off-by: Alcapone-Fx <navasfx19@gmail.com>

---
+++ app/src/main/java/com/example/medicalrecords/CreaExpedienteActivity.java
@@ -0,0 +1,34 @@
+package com.example.medicalrecords;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class CreaExpedienteActivity extends AppCompatActivity {
+
+    ImageButton buttonBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crea_expediente);
+
+        // instanciando el tipo Button a partir del button creado en el archivo XML
+        //se realiza la busqueda por id en archivo R donde se encuentran todos los elementos de la app
+        buttonBack = (ImageButton) findViewById(R.id.imageButtonBack);
+
+        //setea una escucha (listener) al hacer click, pasa como parametro la interfaz y la implementa
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override//sobre escritura del metodo
+            public void onClick(View v) {//las acciones al realizar el click
+                Intent intentBack = new Intent(CreaExpedienteActivity.this, LoginActivity.class);
+                startActivity(intentBack);
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/medicalrecords/LoginActivity.java
+++ app/src/main/java/com/example/medicalrecords/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.medicalrecords;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -188,6 +189,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
+
+        // creacion de Intent para abrir el activity de la creacion de expedientes
+        Intent intentNuevoExpediente = new Intent(LoginActivity.this, CreaExpedienteActivity.class);
+        startActivity(intentNuevoExpediente);// inicio a la actividad objetivo
     }
 
     private boolean isEmailValid(String email) {
@@ -333,7 +338,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                finish();//modificar
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
